7c10c939a9a0fc91d3029b238274f8d614ecd09d
Amjad <amjidrafique786@gmail.com>
2024-11-28 09:27:06 +0500

    some fixes

--- blogs/models.py
+++ blogs/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import ForeignKey
+
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
+
     def __str__(self):
-        return self.title
+        return f"{self.title} by {self.author.username}"
